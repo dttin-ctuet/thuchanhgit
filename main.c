95c85619c110a97a7997e244d91c77a7e98ce784
dttin-ctuet <dttin@ctuet.edu.vn>
2025-10-21 08:38:36 +0700

    final

--- main.c
+++ main.c
@@ -1,8 +1,34 @@
 #include "mylib.h"
 
+void menu(){
+    printf("*** MENU ***\n");
+    printf("1- Xem ngay cua thang\n");
+    printf("2- Kiem tra nam nhuan\n");
+    printf("------------------\n");
+    printf("chon STT chuc nang: \n");
+    printf("------------------\n");
+    int c;
+    scanf("%d",&c);
+
+    switch (c)
+    {
+    case 1:
+        namnhuan();
+        menu();
+        break;
+    case 2:
+        xemthang();
+        menu();
+        break;
+    
+    default:
+        menu();
+        break;
+    }
+
+}
+
 int main(){
-    printf("hello");
-    namnhuan();
-    xemthang();
+    menu();
     return 0;
 }
